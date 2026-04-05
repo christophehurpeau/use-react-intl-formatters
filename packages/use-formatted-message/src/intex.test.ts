78327c3d72fb074269beb98bd38bc79e85b1bcf6
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-04-05 15:11:38 +0200

    chore: update dev dependencies

--- packages/use-formatted-message/src/intex.test.ts
+++ packages/use-formatted-message/src/intex.test.ts
@@ -3,7 +3,6 @@ import { test } from "node:test";
 // eslint-disable-next-line import-x/extensions
 import { useFormattedMessage } from "./index.ts";
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 test("useFormattedMessage should be a function", () => {
   equal(typeof useFormattedMessage, "function");
 });
